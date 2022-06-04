bca9b6bd531d43ac5eb7a8fa49cc2120a4746f93
xLeay <LarkyRiver@gmail.com>
2022-06-04 18:31:42 +0200

    test fetch

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const searchIcon = document.querySelector('.search_icon');
 const searchInput = document.getElementById("js-search_input");
 
+
 function showSearch() {
     searchIcon.classList.toggle('search_icon--active');
 }
@@ -12,3 +13,13 @@ searchInput.oninput = function () {
         searchIcon.classList.remove('search_icon--active');
     }
 };
+
+async function getData(url) {
+    const response = await fetch(url);
+    const data = await response.json();
+    // const text = await data.text();
+    return data;
+}
+
+getData('https://champmastery.xleay.workers.dev/api/?region=europe&endpoint=/lol/match/v5/matches/by-puuid/{puuid}/ids&puuid=bDL6N71AYFjleZiWPBkTxAwZplB1itxIfXRn8w9hvzgdobjO7BvLpXZIhh6OTExkFJFS4npI7ZQfxg&count=10')
+
